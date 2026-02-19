3188e0905d5929d86b58b87f114e32b9bed4014b
AhmedKhalil08 <ahmed.mohamed.hamdy08@gmail.com>
2026-02-19 14:14:41 +0200

    Updated The JSONS

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-// APP ENTRY
-// Runs on every page if needed
 
-
-//Ex:
-// import { initNavbar } from "../modules/navbar/navbar.js";
-// import { checkAuth } from "./auth-guard.js";
-
-// initNavbar();
-// checkAuth();
 
 console.log("App started");
 
-// TODO:
-// global init
-// load navbar partial (optional)
-// check auth state
+        function createSnowflake() {
+            const snowflake = document.createElement('div');
+            snowflake.className = 'snowflake';
+            snowflake.innerHTML = '❅';
+            snowflake.style.left = Math.random() * window.innerWidth + 'px';
+            snowflake.style.animationDuration = (Math.random() * 5 + 5) + 's';
+            
+            // Optional: Vary the opacity for each snowflake (between 30% and 70%)
+            snowflake.style.opacity = Math.random() * 0.4 + 0.3;
+            
+            document.body.appendChild(snowflake);           
+            setTimeout(() => {
+                snowflake.remove();
+            }, 10000);
+        }
+        // Start snow automatically when page loads
+        setInterval(createSnowflake, 800);
