6476663c8a5391844dcc7b109266bd0df7e5b869
AhmedKhalil08 <ahmed.mohamed.hamdy08@gmail.com>
2026-02-21 01:39:56 +0200

    started the product catalog

--- js/modules/products/catalog.js
+++ js/modules/products/catalog.js
@@ -1,16 +1,34 @@
-// PRODUCT CATALOG MODULE
-// Owner: Catalog Dev
-
-// TODO:
-// - fetch products.json
-// - render product cards
-// - search + filter
-
 console.log("catalog module loaded");
 
-//Example of fetching products data and rendering (to be implemented in render function) :
-// fetch("/data/products.json")
-//   .then(r => r.json())
-//   .then(products => {
-//     render(products);
-//   });
+function getAllProducts(){
+    const AllProducts = localStorage.getItem("products");
+    return AllProducts? JSON.parse(AllProducts) : [];
+}
+function viewAllProducts(){
+    const Products = getAllProducts();
+    const productsContainer = document.getElementById("catalogProducts");
+    productsContainer.innerHTML= Products.map(product=>{
+        return `
+        <div class="catalogProductCard">
+        <img src="${product.image}" alt="${product.name}"class="catalogproductImage">
+            <div class="catalogProductDetails">
+                <h6>${product.name}</h6>
+                    <div>
+                    Price : ${product.price}$
+                    </div>
+                    <button class="homeShowDetails" onclick="viewProductDetails('${product.id}')">
+                    Show Details
+                    </button>
+            </div>
+        </div>
+        `
+
+    }).join('');
+}
+//viewAllProducts();
+function viewProductDetails(productID){
+    window.location.href=`./pages/shop/product-details.html?id=${productID}`;
+}
+function creatingCatalogPagination(){
+    const numberOfPages= 
+}
